487375f7659ede64e40384ca82700f4ec7e10b9c
Amir Gafr <amirgafr@gmail.com>
2024-08-11 18:39:41 +0300

    a

---
+++ problem solving 433.js
@@ -0,0 +1,84 @@
+//1- Check if a given number is less than or equal to 100
+let num = 10;
+if(num <= 100){
+  console.log('number is less than or equal to 100');
+}else{
+  console.log('number is greater than 100');  
+}
+
+// 2- Check the number of characters in text
+let char = 'ahmed';
+if(char.length > 100){
+  console.log('string is long');
+}else{
+  console.log('string is short');  
+}
+
+
+
+// 3- Password validation
+if (password === confirmPassword) {
+  console.log('Password validation is valid');
+} else {
+  console.log('Password validation is not valid');
+}
+
+// 4- Check the day of the week if saterday 
+if (password === confirmPassword) {
+  console.log('Password validation is valid');
+} else {
+  console.log('Password validation is not valid');
+}
+
+
+// Check the day of the week if saterday
+let dayOfWeek = 'satarday'
+if (dayOfWeek === 'satarday') {
+  console.log('It\'s the weekend!');
+} else {
+  console.log('It\'s a work day.');
+}
+
+// 5- Check number positive or negative
+let numb = 10;
+if(numb >= 1){
+  console.log('positive number');
+}else{
+  console.log('negative number');  
+}
+
+//6- Verifying academic grade less than 50 F  Greater 60 it's D 70 C 80 B greater than it A
+if (grade >= 90) {
+  return "A";
+} else if (grade >= 80) {
+  return "B";
+} else if (grade >= 70) {
+  return "C";
+} else if (grade >= 60) {
+  return "D";
+} else {
+  return "F";
+}
+// 7- Check temperature temp greater than 30 it's hot else it's cold
+let deg = 10;
+if(numb > 30){
+  console.log('it is hot');
+}else{
+  console.log('it is cold');  
+}
+
+// 8- Checking an odd or even number
+if( num % 2 === 0){
+  console.log('it is even');
+}else{
+  console.log('it is odd');  
+}
+
+// number if greater than 20  console log You can enter the university Else You can't enter
+if( num > 20){
+  console.log('You can enter the university');
+}else{
+  console.log('You can't enter');  
+}
+
+
